e364ca60c65ecaf5692c59afcd4d86d6946b8872
Carlos Agudelo <charlidagudelo@gmail.com>
2024-03-05 20:30:34 -0500

    trabajo en clase 05-03-2024

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,122 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-namespace App\Http\Controllers;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    </nav>
+    <h1 class="text-center m-5">Detalle del producto</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <img class="img-fluid" src="{{ asset('u/comp.png'}}" alt="">
+            </div>
+            <div class="col-md-4">
+                <h1>Computador portatil</h1>
+                <p>jvblfrbvlebvlbflbfnbnfudhuishbrenbsbfiubn</p>
+                <h3>$5.000.000</h3>
+                <button type="button" class="btn btn-primary"> Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('u/u1 .png') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('u/u2.png') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('u/u3.png') }}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <h1 class="text-center m-5">Productos</h1>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-4 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('u/comp.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Computador</h5>
+                      <p class="card-text">$5.000.000</p>
+                      <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('u/comp.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Computador</h5>
+                      <p class="card-text">$5.000.000</p>
+                      <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('u/comp.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Computador</h5>
+                      <p class="card-text">$5.000.000</p>
+                      <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('u/comp.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Computador</h5>
+                      <p class="card-text">$5.000.000</p>
+                      <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('u/comp.png') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Computador</h5>
+                      <p class="card-text">$5.000.000</p>
+                      <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>    
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, ValidatesRequests;
-}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -12,46 +12,21 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+    </nav>
+    <h1 class="text-center m-5">Detalle del producto</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <img class="img-fluid" src="{{ asset('u/comp.png'}}" alt="">
+            </div>
+            <div class="col-md-4">
+                <h1>Computador portatil</h1>
+                <p>jvblfrbvlebvlbflbfnbnfudhuishbrenbsbfiubn</p>
+                <h3>$5.000.000</h3>
+                <button type="button" class="btn btn-primary">Añadir al carrito</button>
             </div>
         </div>
-    </nav>
+    </div>
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
